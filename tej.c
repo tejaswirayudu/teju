8636760cea77ef9f5d89cd36155245abcfef4207
tejaswirayudu <31399905+tejaswirayudu@users.noreply.github.com>
2019-08-08 13:20:46 +0530

    Add files via upload

---
+++ tej.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+int main()
+
+{
+    
+int x,p,s=0,i;
+   
+ scanf("%d %d",&x,&p);
+    
+p=100-p;
+    
+while(x)
+    {
+       
+ s=s+x;
+       
+ x=(x*p)/100;
+    
+}
+  
+  printf("%d",s); 
+    
+return 0;
+}
